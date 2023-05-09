12c121172a1d3675839b3b0d675bd8c5fab8e3c1
CookyNdi <yandikun145@gmail.com>
2023-05-09 19:36:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const moduleBiodata = require("./moduleBiodata");
+
+moduleBiodata.showBiodata("Yandi Hirniyansyah", 19, "Ciamis, 28 Juni 2003", "Ciamis, Jawa Barat");
---
+++ moduleBiodata.js
@@ -0,0 +1,6 @@
+exports.showBiodata = function (name, age, ttl, address) {
+  console.log("Nama: " + name);
+  console.log("TTL: " + ttl);
+  console.log("Usia: " + age);
+  console.log("Alamat: " + address);
+};
